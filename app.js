59f2afe7bf1011f4a82730f64b45b21fa1cd0ff8
shifaza <shifatsar@gmail.com>
2016-12-11 22:29:54 +0500

    Images added, bugs fixed (except json edit)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module('TourOfHeroes', ["ngRoute"]);
 
-//-------------routes
+
+//=====   ROUTES   =====//
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when("/home", {
 		templateUrl : "Partials/home.html",
@@ -17,9 +18,11 @@ app.config(['$routeProvider', function ($routeProvider) {
 		controller : "AliensEditController"
 	})
 
-	.otherwise("/home"); //---------change to home once json is fixed
+	.otherwise("/home");
 }]);
 
+
+//=====   HOME CONTROLLER   =====//
 app.controller('HomeController', ['$scope', '$location', 'alienDataService', function($scope, $location, alienDataService){
     alienDataService.getAliens().then(function(aliens){
       $scope.aliens = aliens.data.aliens;
@@ -27,6 +30,8 @@ app.controller('HomeController', ['$scope', '$location', 'alienDataService', fun
   })
 }]);
 
+
+//=====   ALIENS CONTROLLER   =====//
 app.controller('AliensController', ['$scope', '$location', 'alienDataService', function($scope, $location, alienDataService){
 	
 	$scope.go = function (path) {
@@ -37,24 +42,31 @@ app.controller('AliensController', ['$scope', '$location', 'alienDataService', f
     	return "aliens/" + alienName + "/edit";
     }
 	
+	$scope.imgLocation = function(alienName){
+    	return "/Images/" + alienName + ".png";
+    }	
+	
   alienDataService.getAliens().then(function(aliens){
       $scope.aliens = aliens.data.aliens;
-
-
   })
 }]);
 
-
+//=====   EDIT CONTROLLER   =====//
 app.controller('AliensEditController', ['$scope', 'alienDataService',  function($scope, alienDataService, $routeParams){
-  alienDataService.getAliens().then(function(aliens){
-      $scope.alien = aliens.data.aliens.get($routeParams);
-     //$rootScope.title = "Edit " + ctrl.person.name;
+	var ctrl = this;
+ 	alienDataService.getAliens().then(function(aliens){
 
+ 	 //edit not working
+	 ctrl.alienEdit = aliens.data.aliens.get($routeParams);
+	 //--- previous code
+	 // $scope.editAlien = aliens.data.aliens.get($routeParams);
+     //$rootScope.title = "Edit " + ctrl.person.name;
   })
 }]);
 
 
 // ---- angular factory and promise code borrowed from: http://plnkr.co/edit/7Oas2T
+// ---- retrieves data from json file.
 app.factory('alienDataService',['$http', '$q', function($http, $q){
   var data = null;
   return {
@@ -77,64 +89,3 @@ app.factory('alienDataService',['$http', '$q', function($http, $q){
 
   } ;
 }]);
-
-
-
-
-
-//====with other crap
-// app.controller('AliensController', ['$scope', 'alienDataService',  function($scope, alienDataService){
-//   // when promise is resolved, set the scope variable
-//   alienDataService.getAliens().then(function(aliens){
-//       // store any data from json that you want on the scope for later access in your templates
-//       $scope.aliens = aliens.data.aliens;
-//       //$scope.custInfo=articles.data.customer;
-//       // var ctrl =this;
-//       // ctrl.allAliens = aliens;
-
-//   })
-// }]);
-
-//===============useless or might reuse code=======================
-// app.factory('aliens', function($http){
-// 	 return {
-//           list: function(callback){
-//             $http.get('data.json').success(callback);
-//           }
-//         };       
-// });
-
-// app.service('AlienCtrl', function($scope, $http) {
-//   $http.get("data.json").then(function (response) {
-//       $scope.myData = response.data.aliens;
-//   });
-// });
-
-// app.service("myProvider", function() { // CHANGED "factory" to "service"
-//     // NOTE that the only function being passed is the object constructor from before
-//     this.getValue = function() {
-//         return "My Value";
-//     };
-// });
-
-
-// app.controller("MyController", function(myProvider) {
-//     console.log("MyController - myProvider: " + myProvider.getValue()); // CHANGED to call getValue()
-// });
-
-// app.service('customersService', function($http, $log) {
-
-// this.getCustomers = function() {
-//     $http({
-//         method : 'POST',
-//         url : 'data.json'
-//     }).success(function(data, status, headers, config) {
-//         $log.log('Done');
-//         angular.forEach(data, function(c) {
-//             $log.log(c.firstName);
-//         });
-//         customers = data;
-//         return customers;
-//     });     
-// };
-// });
